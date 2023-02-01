0c9f1c2c985e5172045da8a0d04f4e662adc807d
developertarikulhazra <tariqulhazra1614@gmail.com>
2023-02-01 12:07:28 +0600

    return fuction done

---
+++ function.js
@@ -0,0 +1,12 @@
+function startfan() {
+  console.log("Stand up");
+  console.log("walk towards the switch");
+  console.log("Press the switch");
+}
+// call the functions
+startfan();
+console.log("Waking up in the morning");
+startfan();
+console.log("Eating lunch");
+console.log("watching js tutoial");
+startfan();
---
+++ paramiter.js
@@ -0,0 +1,26 @@
+function bringSingara(money) {
+  console.log("mama singra den");
+  console.log(money);
+}
+bringSingara(250);
+
+function bringsSingra(money) {
+  console.log("eto taka desen", money);
+  console.log("Ai nen Singara");
+}
+
+var taka = 300;
+bringsSingra(taka);
+
+function add(number1, number2) {
+  console.log("going to add", number1, number2);
+}
+add(125, 96);
+
+function sum(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  var sum = a + b + c + d + e;
+  console.log(sum);
+}
+
+sum(10, 200, "300", 500, 600);
